8289e41ec30eab2518acffe8c5fd72faba3d5cb8
Cory-Christensen <christensen.corym@gmail.com>
2016-09-15 19:06:29 -0600

    Removed registration

--- client/js/routes.jsx
+++ client/js/routes.jsx
@@ -6,16 +6,12 @@ import { Router, Route, IndexRoute }  from 'react-router';
 import appHistory                     from './history';
 import Index                          from './components/layout/index';
 import Home                           from './components/home';
-import Register                       from './components/register/register';
 import NotFound                       from './components/common/not_found';
 
 export default (
   <Router history={appHistory}>
     <Route path='/' component={Index}>
       <IndexRoute component={Home} />
-    </Route>
-    <Route path='/register' component={Index}>
-      <IndexRoute component={Register} />
     </Route>
     <Route path='*' component={NotFound} />
   </Router>
